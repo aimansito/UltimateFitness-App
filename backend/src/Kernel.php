9ce99ceb355b975318acb7e52bf40608e99ec74e
Aiman <ahardao1001@g.educaand.es>
2026-01-31 23:27:14 +0100

    force API platform

--- backend/src/Kernel.php
+++ backend/src/Kernel.php
@@ -8,4 +8,22 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function registerBundles(): iterable
+    {
+        $contents = require $this->getProjectDir().'/config/bundles.php';
+        $apiPlatformFound = false;
+        foreach ($contents as $class => $envs) {
+            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
+                if ($class === \ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class) {
+                    $apiPlatformFound = true;
+                }
+                yield new $class();
+            }
+        }
+
+        if (!$apiPlatformFound && class_exists(\ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class)) {
+            yield new \ApiPlatform\Symfony\Bundle\ApiPlatformBundle();
+        }
+    }
 }
